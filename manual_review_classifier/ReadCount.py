abca1001d079b02d105c5546bcf7f33c85b348db
Ben Ainscough <bainscou@genome.wustl.edu>
2017-04-14 16:07:59 -0500

    Parse single bamreadcount file into pandas dataframe

--- manual_review_classifier/ReadCount.py
+++ manual_review_classifier/ReadCount.py
@@ -1,5 +1,15 @@
 import re
+import pandas as pd
 from manual_review_classifier.utils import to_numeric
+
+
+BASE_METRICS = ['count', 'avg_mapping_quality', 'avg_basequality',
+                'avg_se_mapping_quality', 'num_plus_strand',
+                'num_minus_strand', 'avg_pos_as_fraction',
+                'avg_num_mismaches_as_fraction', 'avg_sum_mismatch_qualities',
+                'num_q2_containing_reads',
+                'avg_distance_to_q2_start_in_q2_reads',
+                 'avg_clipped_length', 'avg_distance_to_effective_3p_end']
 
 class ReadCount:
     """Parse bam-readcount out into dict or pandas.Dataframe
@@ -13,7 +23,7 @@ class ReadCount:
             file_path (str): File path of bam-readcount file  
         """
         self.read_count_dict = self._parse(file_path)
-
+        self.read_count_df = pd.DataFrame()
 
     def _parse(self, file_path):
         """Read and parse the bam_readcount file into a dict
@@ -27,12 +37,10 @@ class ReadCount:
         file = open(file_path)
         counts = {}
         for line in file:
-            match = re.match(r'(^\w+\t\d+\t\w\t\d+)', line)
+            match = re.match(r'(^[\w|.]+\t\d+\t\w\t\d+)', line)
             if match is not None:
                 count = re.split('\t|:', line.strip())
-                position = '{0}:{1}{2}'.format(count[0],
-                                               count[1],
-                                               count[2])
+                position = '{0}:{1}'.format(count[0], count[1])
                 metrics = {}
                 metrics['chromosome'] = count[0]
                 metrics['position'] = int(count[1])
@@ -40,21 +48,85 @@ class ReadCount:
                 metrics['depth'] = int(count[3])
                 bases = {}
                 for i in range(4, len(count), 14):
-                    base_metrics = ['count', 'avg_mapping_quality'
-                                             'avg_basequality',
-                                    'avg_se_mapping_quality',
-                                    'num_plus_strand', 'num_minus_strand',
-                                    'avg_pos_as_fraction',
-                                    'avg_num_mismaches_as_fraction',
-                                    'avg_sum_mismatch_qualities',
-                                    'num_q2_containing_reads',
-                                    'avg_distance_to_q2_start_in_q2_reads',
-                                    'avg_clipped_length',
-                                    'avg_distance_to_effective_3p_end']
                     b = list(map(to_numeric, count[i + 1: i + 14]))
-                    if not all(x == 0 for x in b):
-                        bases[count[i]] = dict(zip(base_metrics, b))
+                    # if not all(x == 0 for x in b):
+                    if count[i] != '=':
+                        bases[count[i]] = dict(zip(BASE_METRICS, b))
                 metrics['bases'] = bases
                 counts[position] = metrics
         return counts
 
+    def compute_variant_metrics(self, var_bed_file_path):
+        """
+        
+        Args:
+            var_bed_file_path (str): filepath to bed file indicating variants
+        
+        Returns:
+             pandas.Dataframe of variant centric read count data 
+        """
+        # TODO write code that specifically handles snvs and indels
+        if len(self.read_count_df) > 0:
+            return self.read_count_df
+        with open(var_bed_file_path, 'r') as f:
+            for line in f:
+                debug = line.strip()
+                line = line.strip().split('\t')
+                chromosome, start, stop, reference, variant = line
+                start = int(start)
+                stop = int(stop)
+                site = '{0}:{1}'.format(chromosome, start)
+
+                # insersions
+                if reference == '-':
+                    self.flatten_base_metrics(self.read_count_dict[site]['ref'], site, 'ref')
+                    self.flatten_base_metrics('+{0}'.format(variant), site, 'var')
+                    # This dropping the counts at other positions could eliminate some real signal
+                    self.remove_extra_indel_counts(chromosome, start, stop)
+                # deletions
+                elif variant == '-':
+                    self.flatten_base_metrics(self.read_count_dict[site]['ref'], site, 'ref')
+                    self.flatten_base_metrics('-{0}'.format(reference), site, 'var')
+                    self.remove_extra_indel_counts(chromosome, start, stop)
+                # snvs
+                else:
+                    self.flatten_base_metrics(reference, site, 'ref' )
+                    self.flatten_base_metrics(variant, site, 'var')
+                other_bases_count = 0
+                for base in self.read_count_dict[site]['bases']:
+                    #TODO check if other base metrics are nearly identical due to proximity
+                    other_bases_count += self.read_count_dict[site]['bases'][base]['count']
+                self.read_count_dict[site]['other_bases_count'] = other_bases_count
+                self.read_count_dict[site].pop('bases')
+                self.read_count_dict[site]['ref'] = reference
+                self.read_count_dict[site]['var'] = variant
+                self.read_count_dict[site]['stop']= stop
+                self.read_count_dict[site]['start'] = self.read_count_dict[site].pop('position')
+        # print(self.read_count_dict)
+        self.read_count_df = pd.DataFrame.from_dict(self.read_count_dict,orient='index')
+        return self.read_count_df
+
+    def remove_extra_indel_counts(self, chromosome, start, stop):
+        for i in range(start + 1, stop +1 ):
+            remove_site = '{0}:{1}'.format(chromosome, i)
+            self.read_count_dict.pop(remove_site)
+
+    def flatten_base_metrics(self, base_key, site, prepend_string):
+        """ Renames the base metrics and flatten the data into the site level
+        
+        :param base_key (str): Key for the reference or variant 
+        :param site (str): Site string 
+        :param prepend_string (str): string to prepend the new keys
+        """
+
+        try:
+            for metric_key in self.read_count_dict[site]['bases'][base_key]:
+                new_key = '{0}_{1}'.format(prepend_string, metric_key)
+                self.read_count_dict[site][new_key] = \
+                    self.read_count_dict[site]['bases'][base_key][metric_key]
+            # Remove ref or var metrics so counts for other bases can be summed
+            self.read_count_dict[site]['bases'].pop(base_key)
+        except KeyError:
+            # add all zero metrics for indels absent from count file
+            keys = ['{0}_{1}'.format(prepend_string, i) for i in BASE_METRICS]
+            self.read_count_dict[site].update(dict.fromkeys(keys, 0))
--- manual_review_classifier/tests/test_readCount.py
+++ manual_review_classifier/tests/test_readCount.py
@@ -2,14 +2,108 @@ from unittest import TestCase
 
 from manual_review_classifier.ReadCount import ReadCount
 
+import pandas as pd
 import pickle
 
+TEST_DATA_ROOT = 'manual_review_classifier/tests/tst1/'
 
 class TestReadCount(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.read_count = ReadCount( TEST_DATA_ROOT + 'tst1_normal.counts')
+
     def test__parse(self):
-        valid_dict = pickle.load(open(
-            'manual_review_classifier/tests/tst1/tst1_normal_valid_dict.pkl',
-            'rb'))
-        test_dict = ReadCount('manual_review_classifier/tests/tst1/' + \
-                              'tst1_normal.counts').read_count_dict
+        valid_dict = pickle.load(open(TEST_DATA_ROOT +
+                                      'tst1_normal_valid_dict.pkl',
+                                      'rb'))
+        test_dict = self.read_count.read_count_dict
         self.assertEqual(valid_dict, test_dict)
+
+    def test_compute_variant_metrics(self):
+        data = self.read_count.compute_variant_metrics(TEST_DATA_ROOT +
+                                                       'tst1_full.bed')
+        print(data.columns)
+        self.assertTrue(all(data.columns ==
+                            ['chromosome', 'ref', 'depth', 'ref_count',
+                             'ref_avg_mapping_quality',
+                             'ref_avg_basequality',
+                             'ref_avg_se_mapping_quality',
+                             'ref_num_plus_strand', 'ref_num_minus_strand',
+                             'ref_avg_pos_as_fraction',
+                             'ref_avg_num_mismaches_as_fraction',
+                             'ref_avg_sum_mismatch_qualities',
+                             'ref_num_q2_containing_reads',
+                             'ref_avg_distance_to_q2_start_in_q2_reads',
+                             'ref_avg_clipped_length',
+                             'ref_avg_distance_to_effective_3p_end',
+                             'var_count',
+                             'var_avg_mapping_quality', 'var_avg_basequality',
+                             'var_avg_se_mapping_quality',
+                             'var_num_plus_strand',
+                             'var_num_minus_strand', 'var_avg_pos_as_fraction',
+                             'var_avg_num_mismaches_as_fraction',
+                             'var_avg_sum_mismatch_qualities',
+                             'var_num_q2_containing_reads',
+                             'var_avg_distance_to_q2_start_in_q2_reads',
+                             'var_avg_clipped_length',
+                             'var_avg_distance_to_effective_3p_end',
+                             'other_bases_count', 'var',
+                             'stop', 'start']))
+# ['chromosome', 'start', 'stop',
+# 'ref', 'var', 'depth','tumor_vaf',
+# 'tumor_var_base_count',
+# 'tumor_var_avg_mapping_quality'
+# 'tumor_var_avg_basequality',
+# 'tumor_var_avg_se_mapping_quality',
+# 'tumor_var_num_plus_strand',
+# 'tumor_var_num_minus_strand',
+# 'tumor_var_avg_pos_as_fraction',
+# 'tumor_var_avg_num_mismaches_as_fraction',
+# 'tumor_var_avg_sum_mismatch_qualities',
+# 'tumor_var_num_q2_containing_reads',
+# 'tumor_var_avg_distance_to_q2_start_in_q2_reads',
+# 'tumor_var_avg_clipped_length',
+# 'tumor_var_avg_distance_to_effective_3p_end',
+# 'tumor_ref_base_count',
+# 'tumor_ref_avg_mapping_quality'
+# 'tumor_ref_avg_basequality',
+# 'tumor_ref_avg_se_mapping_quality',
+# 'tumor_ref_num_plus_strand',
+# 'tumor_ref_num_minus_strand',
+# 'tumor_ref_avg_pos_as_fraction',
+# 'tumor_ref_avg_num_mismaches_as_fraction',
+# 'tumor_ref_avg_sum_mismatch_qualities',
+# 'tumor_ref_num_q2_containing_reads',
+# 'tumor_ref_avg_distance_to_q2_start_in_q2_reads',
+# 'tumor_ref_avg_clipped_length',
+# 'tumor_ref_avg_distance_to_effective_3p_end',
+# 'tumor_other_bases_count',
+# 'normal_vaf',
+# 'normal_var_base_count',
+# 'normal_var_avg_mapping_quality'
+# 'normal_var_avg_basequality',
+# 'normal_var_avg_se_mapping_quality',
+# 'normal_var_num_plus_strand',
+# 'normal_var_num_minus_strand',
+# 'normal_var_avg_pos_as_fraction',
+# 'normal_var_avg_num_mismaches_as_fraction',
+# 'normal_var_avg_sum_mismatch_qualities',
+# 'normal_var_num_q2_containing_reads',
+# 'normal_var_avg_distance_to_q2_start_in_q2_reads',
+# 'normal_var_avg_clipped_length',
+# 'normal_var_avg_distance_to_effective_3p_end',
+# 'normal_ref_base_count',
+# 'normal_ref_avg_mapping_quality'
+# 'normal_ref_avg_basequality',
+# 'normal_ref_avg_se_mapping_quality',
+# 'normal_ref_num_plus_strand',
+# 'normal_ref_num_minus_strand',
+# 'normal_ref_avg_pos_as_fraction',
+# 'normal_ref_avg_num_mismaches_as_fraction',
+# 'normal_ref_avg_sum_mismatch_qualities',
+# 'normal_ref_num_q2_containing_reads',
+# 'normal_ref_avg_distance_to_q2_start_in_q2_reads',
+# 'normal_ref_avg_clipped_length',
+# 'normal_ref_avg_distance_to_effective_3p_end',
+# 'normal_other_bases_count']
