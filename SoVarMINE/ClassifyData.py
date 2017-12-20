dd3b9a495c7e33a95038f4d9bb5cf162c83a3c44
Erica Barnell <ebarnell@GSCs-MacBook-Pro-2.local>
2017-12-20 10:26:18 -0600

    create scripts for classifier use
    Former-commit-id: 0f72d2338d383a9a12642800079507b7580639a1 [formerly 2b8f908fe9967d2fbf88fc7ea18c7367395abc3b] [formerly 3d57d06df8e70c26998af061fb74ce78cf21ee04 [formerly d7ef7f6c881c0c1956fbbcfd3297037b5891e5b2]]
    Former-commit-id: ca439544c4a538488aae81070d1f726836c0242d
    Former-commit-id: 9dcac1993af1a67a7bc8ea440837c5f51ac911d8

---
+++ SoVarMINE/ClassifyData.py
@@ -0,0 +1,75 @@
+import os
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import preprocessing
+
+
+class ClassifyData:
+    """Input processed data to classify variants as: Somatic, Ambigious, or Fail 
+
+
+    """
+
+    def __init__(self, processed_data, classifier, solid_tumor):
+        """Assemble pandas.Dataframe of data
+
+            Args:
+                processed_data (str): File path of tab-separated
+                                         file outlining the tumor bam path,
+                                         normal bam path, and manual review
+                                         sites file path (this should be a
+                                         one-based tsv file containing
+                                         chromosome, start, and stop),
+                                         disease, reference fasta file path
+                classifier (str): trained classifier located in the folder
+                solid_tumor (bool): True if solid tumor, False otherwise (i.e. hematologic tumor).
+        """
+        self._parse_samples_file(samples_file_path, header)
+        self.classify_samples(samples, solid_tumor, classifier)
+        self.processed_data = processed_data
+
+    def parse_samples(processed_data):
+        """parse processed data to return samples
+
+            Args:
+                processed_data (str): File path of tab-separated
+                                         file outlining the tumor bam path,
+                                         normal bam path, and manual review
+                                         sites file path (this should be a
+                                         one-based tsv file containing
+                                         chromosome, start, and stop),
+                                         disease, reference fasta file path
+        """
+        
+        with open(processed_data) as f:
+            samples = f.readlines()
+            samples = [x.strip() for x in samples]
+            samples = [x.split('\t') for x in samples]
+        self.samples = samples
+        
+    def classify_samples(classifier, solid_tumor, processed_data):
+        """classify processed data using classifier
+
+            Args:
+                classifier (str): trained classifier located in the folder
+        """
+        
+        model = model_from_json(open('deep_learning_classifier.json').read())
+        
+        if solid_tumor:
+            processed_data['solid_tumor'] = 1
+        else:
+            processed_data['solid_tumor'] = 0
+        
+        X = samples.sort_index(axis=1).astype(float).values
+        
+        return estimator.predict_proba(X)
+        
+        
+        
+        
+        
+        
+        
+        
