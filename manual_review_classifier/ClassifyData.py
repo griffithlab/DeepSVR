ba85261934049d62715f07bbdc33043e0d994ea0
ebarnell <erica.barnell@gmail.com>
2018-02-23 13:30:40 -0600

    update the mrc and the re-review analysis
    Former-commit-id: 11ba02c5193a61643bd3fc31462a8c730cd28ff4 [formerly 7c1624af023de4a56fbc114f04c22e109020accb] [formerly 7dd325b3e2d9ab58fc6720319d316a6e943134ac [formerly dc7315c95ccdbe4a3af2de1dc335275ce2fd398a] [formerly 72857fbc5f1b107e54502fe1fa96ad76424af356 [formerly 0ac6313433cbae5944dfa1d7508999f47d06a071] [formerly f02ff9a868d5dc23ed9f88873c6d292c82f67be4 [formerly c1aed1a307d6422d93a4e3047a6cf126c04eb280 [formerly 8d31cf812ecec61d59a4b8fd77ca48cf04a374e3]]]]]
    Former-commit-id: 63e230016a1184cffe15287f624e6b384d3e2859 [formerly b16e1bd57dee2dc72b3d2dc2e31c6c2e3eb0bb20] [formerly e9093c0bca8e9e3d31d48058f6bf7e015725f4d0] [formerly 2df1e815acf5d2845380a4d79caa82d5b3680363 [formerly 44409713f210d741aec0f6223f19e7a7aa8459d4] [formerly 6182efafc6f618f58e88ef9431dee50903be792d]] [formerly db77f38369f8cfb4ec5f3fe7b5f660e4500e2c2a]
    Former-commit-id: 496d778070e7c040c507ef69a09350d5bfd53569
    Former-commit-id: e9acb6a6a3d3af648c23c9ca6b7e90e1133a5c35 [formerly 159ff1f13095ca11b5e0c1bc8e5e3f19774fc661] [formerly eff0ec2952573fa7669bc4840eda0b78d6a145a3]
    Former-commit-id: f4814f2d92e220ca2bcc57320bcc47f403f327f6
    Former-commit-id: cafd7ae5f8e97bdf8aa3531c7467e99c9f48ff40

--- manual_review_classifier/ClassifyData.py
+++ manual_review_classifier/ClassifyData.py
@@ -1,10 +1,16 @@
 import os
 import pandas as pd
 import numpy as np
-import sklearn
-
-
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.regularizers import l2
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_predict
 from sklearn import preprocessing
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from keras.models import model_from_json
 
 
 class ClassifyData:
@@ -13,62 +19,79 @@ class ClassifyData:
 
     """
 
-    def __init__(self, processed_data, classifier, hematologic_tumor):
+    def __init__(self, solid_tumor, classifier, samples_file_path, header, output_dir_path):
         """Assemble pandas.Dataframe of data
 
             Args:
-                processed_data (str): File path of tab-separated
+                samples_file_path (str): File path of tab-separated
                                          file outlining the tumor bam path,
                                          normal bam path, and manual review
                                          sites file path (this should be a
                                          one-based tsv file containing
                                          chromosome, start, and stop),
                                          disease, reference fasta file path
-                classifier (str): trained classifier located in the folder
+                header (bool): True if header False otherwise.
                 solid_tumor (bool): True if solid tumor, False otherwise (i.e. hematologic tumor).
-        """
-        self._parse_samples_file(samples_file_path, header)
-        self.out_dir_path = out_dir_path
-        self.training_data = pd.DataFrame()
-        self.categorical_columns = list()
-        self._run_bam_readcount(skip_readcount)
-
-    def parse_samples(processed_data):
-        """parse processed data to return samples
-
-            Args:
-                processed_data (str): File path of tab-separated
-                                         file outlining the tumor bam path,
-                                         normal bam path, and manual review
-                                         sites file path (this should be a
-                                         one-based tsv file containing
-                                         chromosome, start, and stop),
-                                         disease, reference fasta file path
+                classifier (str): path for classifier (default is 'deep_learning_classifier.json')
+                out_dir_path (str): path for output
+                
         """
         
-        with open(processed_data) as f:
+        self.output_dir_path = output_dir_path
+        self._parse_samples_file(samples_file_path, header)
+        self._classify_samples(solid_tumor, classifier, output_dir_path)
+        
+        
+
+    def _parse_samples_file(self, samples_file_path, header):
+        """Parse samples
+
+            Args:
+                samples_file_path (str): File path of tab-separated
+                                         file outlining the tumor bam path,
+                                         normal bam path, and manual review
+                                         sites file path (this should be a
+                                         one-based tsv file containing
+                                         chromosome, start, and stop),
+                                         disease, reference fasta file path
+                header (bool): True if header False otherwise.
+        """
+        with open(samples_file_path) as f:
             samples = f.readlines()
             samples = [x.strip() for x in samples]
             samples = [x.split('\t') for x in samples]
+            if header:
+                samples.pop(0)
         self.samples = samples
-        
-    def classify_samples(classifier):
+
+    
+    def _classify_samples(self, solid_tumor, classifier, output_dir_path):
         """classify processed data using classifier
 
             Args:
-                classifier (str): trained classifier located in the folder
+                solid_tumor (bool): True if solid tumor, False otherwise (i.e. hematologic tumor).
+                classifier (str): path for classifier (default is 'deep_learning_classifier.json')
+                out_dir_path (str): path for output
+
         """
         
-        model = model_from_json(open('deep_learning_classifier.json').read())
+        estimator = model_from_json(open('deep_learning_classifier.json').read())
         
+        processed_data = pd.read_pickle('output/train.pkl')
+
         if solid_tumor:
             processed_data['solid_tumor'] = 1
         else:
             processed_data['solid_tumor'] = 0
+        X = processed_data.sort_index(axis=1).drop(['disease_BRC','reviewer_1'], 
+            axis =1).astype(float).values
         
-        X = samples.sort_index(axis=1).astype(float).values
+        probabilities = estimator.predict_proba(X)
         
-        return estimator.predict_proba(X)
+        out_dir_path = output_dir_path
+        predictions = pd.DataFrame(probabilities)
+        predictions.to_csv(out_dir_path + "predictions.tsv")
+
         
         
         
