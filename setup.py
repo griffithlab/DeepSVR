a81b582810ac7990e20985536cf97643b9535135
Alex H. Wagner, PhD <awagner24@wustl.edu>
2018-04-02 16:07:20 -0500

    lowercase deepSVR
    Former-commit-id: ef3f364465aab1d7503af146eb4df24737394e58 [formerly 6550c5c64e925e5b2cce6fcd1a2b7ae7d86af5aa] [formerly c4c2c6733d0c5de6e731674e47b0013a660f0d09 [formerly 2ecd908e9b96fe4394143c23848c2313f66c68d8] [formerly 56446ad6c0a5cdf4a2afdeeb599dd357024f3d54 [formerly 16ba42df02fc4ab56342c508c80a57068e63bc69 [formerly 47dd43e72ae835ee1c4def30901b576fcc154120]]]]
    Former-commit-id: 4cc57d54f470e823d8f9c0fcd3e53d46a114b246
    Former-commit-id: 33fd5fe458f4a4f612c3fbeb0885cf589a8ce8d3 [formerly 3c4720da18db3e18be2681c62d9a1fd9c5ba8bc5] [formerly 0eba847db8659e395378561abf550e390d8377cf]
    Former-commit-id: 8deab30a46d0067fe03d1e60c6485755cce2d587
    Former-commit-id: 336faf0348827dcbe8630eec9b81d8a653acdb3f

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='DeepSVR',  # Required
+    name='deepSVR',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
@@ -61,7 +61,7 @@ setup(
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/griffithlab/manual_review_classifier/tree/master/DeepSVR',  # Optional
+    url='https://github.com/griffithlab/manual_review_classifier/tree/master/deepSVR',  # Optional
 
     # This should be your name or the name of the organization which owns the
     # project.
@@ -161,8 +161,8 @@ setup(
     # executes the function `main` from this package when invoked:
     entry_points={  # Optional
         'console_scripts': [
-            'deepsvr=DeepSVR.cli:main',
-            'createclassifier=DeepSVR.CreateClassifier:main'
+            'deepsvr=deepSVR.cli:main',
+            'createclassifier=deepSVR.CreateClassifier:main',
         ],
     },
 
