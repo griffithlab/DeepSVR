c550c77d340d449bc2b2a2fc789a285a604caef5
Alex H. Wagner, PhD <awagner24@wustl.edu>
2018-04-02 09:17:56 -0500

    switch to DeepSVR setup
    Former-commit-id: 4634cd58030b2cf83a7410d68110f5f005d64ced [formerly 47911025a858690d73387bec76da596a926d4fa7] [formerly fe49197574c9abf9c65e9cf8e8f3f0049a8198f7 [formerly cdabddd2e8f59d9d2368578358d59618cc11fe68] [formerly 9d18e742d11d1b912e239111a8fb4243a83f8e44 [formerly e8da1fc8e3f721fe24bc8d9a9a842e3d5f8dd4cc [formerly cda73d41e853ad54b500658c65e6703fee381029]]]]
    Former-commit-id: 3fed69d2461d213238b81ca119b68f1b32bebccd
    Former-commit-id: 491e5dbcdd9d4616b609bd9d57781da75cb3081a [formerly bdfdf8c9897682256f5a5a7d142240eba34b50ad] [formerly 7842689d5abf7c480754c9985e7969a26aa4e30b]
    Former-commit-id: 4855848535442b41ebc1a4d2e6632edc7fe0a389
    Former-commit-id: 78b0f05d25408a5765f28f490b9773d2a19cd6ed

---
+++ manual_review_classifier/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='manual_review_classifier',
+    version='0.0.1',
+    description='Manual review processing',
+    url='https://github.com/bainscou/manual_review_classifier',
+    author='Ben Ainscough',
+    author_email='b.ainscough@wustl.edu',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning'
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License'
+        'Programming Language :: Python :: 3.6'
+    ],
+    packages=find_packages()
+)
