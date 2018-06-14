ba5b21747d31763b050492f0aebeac0c0bfa94e7
Ben Ainscough <bainscou@genome.wustl.edu>
2018-06-13 20:24:08 -0600

    Fix codestyle: flake8 passing
    Former-commit-id: 541cc000ef442c27fc28c4c4a63c03dc4d3e5921 [formerly ba3256266c08bf58f81f7f5db2cadb9826ee0c6f [formerly 11e0889a41e9fd47db0a86f66d06347405b5235d [formerly bd220f4a1cc17bfb379db69b6ee5ad8527feff54]]]
    Former-commit-id: c20c81df83ce540d4a56a9064e6b239075c7b061
    Former-commit-id: 0b3146dc09aaaaa40f7682562c950f39b066602b
    Former-commit-id: cc56ad3172031e97cdd273adc90aa805926a678a

--- deepsvr/ClassifyData.py
+++ deepsvr/ClassifyData.py
@@ -1,35 +1,29 @@
 import os
 import pandas as pd
-import numpy as np
-import sklearn
-from sklearn import preprocessing
 from keras.models import model_from_json
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 class ClassifyData:
-    """Input processed data to classify variants as: Somatic, Ambigious, or Fail 
-
-
+    """Input processed data to classify variants as: Somatic, Ambiguous,
+    or Fail
     """
 
     def __init__(self, solid_tumor, sample_file_path, header):
         """Assemble pandas.Dataframe of data
 
             Args:
-                solid_tumor (bool): True if solid tumor, False otherwise (i.e. hematologic tumor).
+                solid_tumor (bool): True if solid tumor, False otherwise
+                                    (i.e. hematologic tumor).
         """
-        
         self.classify_samples(solid_tumor, sample_file_path, header)
-    
-    
+
     def classify_samples(self, solid_tumor, sample_file_path, header):
         """classify processed data using classifier
 
             Args:
                 classifier (str): trained classifier located in the folder
-           
+
                 samples_file_path (str): File path of tab-separated
                              file outlining the tumor bam path,
                              normal bam path, and manual review
@@ -44,31 +38,39 @@ class ClassifyData:
         loaded_model_json = json_file.read()
         json_file.close()
         loaded_model = model_from_json(loaded_model_json)
-        
+
         # load weights into new model
         loaded_model.load_weights("data/model.h5")
         print("Loaded model from disk")
         print()
-    
+
         processed_data = pd.read_pickle('Output/train.pkl')
-        processed_data = processed_data[processed_data.columns.drop(list(processed_data.filter(regex='disease')))]
-        
+        disease_cols = processed_data.columns.drop(list(processed_data.filter(
+            regex='disease')))
+        processed_data = processed_data[disease_cols]
+
         if solid_tumor:
             processed_data['solid_tumor'] = 1
         else:
             processed_data['solid_tumor'] = 0
-            
+
         X = processed_data.sort_index(axis=1).astype(float).values
-        
-        calls = pd.read_pickle('Output/call.pkl')
-        Y = pd.get_dummies(calls).astype(float).values
-        
+
+        # calls = pd.read_pickle('Output/call.pkl')
+        # Y = pd.get_dummies(calls).astype(float).values
         probabilities = loaded_model.predict_proba(X)
-        
-        probs_df = pd.DataFrame(probabilities, columns=['Ambiguous', 'Fail', 'Somatic'], index=processed_data.index)
-        probs_df['Max'] = probs_df[['Ambiguous', 'Fail', 'Somatic']].max(axis=1)
-        probs_df['Call'] = pd.np.where(probs_df["Max"] == probs_df["Ambiguous"], "A",
-                   pd.np.where(probs_df["Max"] == probs_df["Somatic"], "S",
-                   pd.np.where(probs_df["Max"] == probs_df["Fail"], "F", 'NONE')))
+        probs_df = pd.DataFrame(probabilities,
+                                columns=['Ambiguous', 'Fail', 'Somatic'],
+                                index=processed_data.index)
+        probs_df['Max'] = probs_df[['Ambiguous',
+                                    'Fail',
+                                    'Somatic']].max(axis=1)
+        probs_df['Call'] = pd.np.where(
+            probs_df["Max"] == probs_df["Ambiguous"], "A",
+            pd.np.where(probs_df["Max"] == probs_df["Somatic"], "S",
+                        pd.np.where(probs_df["Max"] == probs_df["Fail"], "F",
+                                    'NONE')
+                        )
+        )
 
         probs_df.to_csv("Output/predictions.tsv", sep='\t', header=True)
