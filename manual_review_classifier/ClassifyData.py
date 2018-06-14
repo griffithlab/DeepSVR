f9bb0f64f7d6b4f7f82e3871ee856a6d464d444d
Ben Ainscough <bainscou@genome.wustl.edu>
2018-06-13 20:24:08 -0600

    Fix codestyle: flake8 passing
    Former-commit-id: 99ddc4178dffd26beea061dc752ff69a2428beaa [formerly 090104905d169996a7a6b6af079edc03400c11a7] [formerly 271257350a09d082a4b24e5b3e4566a550a07617 [formerly 541cc000ef442c27fc28c4c4a63c03dc4d3e5921] [formerly aa5a51b193d85a32583b14c55f5093bc3603d5e1 [formerly 52b74cff5e41d23f78ddb30435a93de390257976] [formerly ba3256266c08bf58f81f7f5db2cadb9826ee0c6f [formerly bd220f4a1cc17bfb379db69b6ee5ad8527feff54]]]]
    Former-commit-id: f6b5c8f97a49f3e0b394ee4ef4dfc4bf0e63f42b [formerly b00fcba8206bd4ef957b3e1f3cd011972318cb2c] [formerly f5f3f2d81cf90c9529da531e3de41a4e1bc20e77] [formerly 27ec38c3f9576e654d9576bc3641ef7f6229245e [formerly cc56ad3172031e97cdd273adc90aa805926a678a] [formerly 0b3146dc09aaaaa40f7682562c950f39b066602b]] [formerly c20c81df83ce540d4a56a9064e6b239075c7b061]
    Former-commit-id: a09068a0c67083ef03da1115367becb04264ad4e
    Former-commit-id: de148553326d0f1c29978a256813c12d57c77a65 [formerly 0585ac70adbf780b6e51d81fa34d93c99d57a779] [formerly d53cb997ec084744c52e71161d7d3911d2da8c79]
    Former-commit-id: e4ca836c15ac4b6489f4f7c1887608de119fc2a1
    Former-commit-id: ca4c89bcc4cde0bed63d6210b410fe3ad3e8c4a8

--- manual_review_classifier/ClassifyData.py
+++ manual_review_classifier/ClassifyData.py
@@ -1,25 +1,14 @@
-import os
 import pandas as pd
-import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.wrappers.scikit_learn import KerasClassifier
-from keras.regularizers import l2
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_predict
-from sklearn import preprocessing
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
 from keras.models import model_from_json
 
 
 class ClassifyData:
-    """Input processed data to classify variants into: Somatic, Ambigious, and Fail 
-
-
+    """Input processed data to classify variants into: Somatic, Ambiguous,
+    and Fail
     """
 
-    def __init__(self, solid_tumor, classifier, samples_file_path, header, output_dir_path):
+    def __init__(self, solid_tumor, classifier, samples_file_path,
+                 header, output_dir_path):
         """Assemble pandas.Dataframe of data
 
             Args:
@@ -31,17 +20,15 @@ class ClassifyData:
                                          chromosome, start, and stop),
                                          disease, reference fasta file path
                 header (bool): True if header False otherwise.
-                solid_tumor (bool): True if solid tumor, False otherwise (i.e. hematologic tumor).
-                classifier (str): path for classifier (default is 'deep_learning_classifier.json')
+                solid_tumor (bool): True if solid tumor, False otherwise
+                                    (i.e. hematologic tumor).
+                classifier (str): path for classifier
+                                  (default is 'deep_learning_classifier.json')
                 out_dir_path (str): path for output
-                
         """
-        
         self.output_dir_path = output_dir_path
         self._parse_samples_file(samples_file_path, header)
         self._classify_samples(solid_tumor, classifier, output_dir_path)
-        
-        
 
     def _parse_samples_file(self, samples_file_path, header):
         """Parse samples
@@ -64,39 +51,30 @@ class ClassifyData:
                 samples.pop(0)
         self.samples = samples
 
-    
     def _classify_samples(self, solid_tumor, classifier, output_dir_path):
         """classify processed data using classifier
 
             Args:
-                solid_tumor (bool): True if solid tumor, False otherwise (i.e. hematologic tumor).
-                classifier (str): path for classifier (default is 'deep_learning_classifier.json')
+                solid_tumor (bool): True if solid tumor, False otherwise
+                                    (i.e. hematologic tumor).
+                classifier (str): path for classifier (default is
+                                  'deep_learning_classifier.json')
                 out_dir_path (str): path for output
 
         """
-        
-        estimator = model_from_json(open('deep_learning_classifier.json').read())
-        
+        estimator = model_from_json(open('deep_learning_classifier.json'
+                                         ).read())
+
         processed_data = pd.read_pickle('output/train.pkl')
 
         if solid_tumor:
             processed_data['solid_tumor'] = 1
         else:
             processed_data['solid_tumor'] = 0
-        X = processed_data.sort_index(axis=1).drop(['disease_BRC','reviewer_1'], 
-            axis =1).astype(float).values
-        
+        X = processed_data.sort_index(axis=1).drop(['disease_BRC',
+                                                    'reviewer_1'], axis=1
+                                                   ).astype(float).values
         probabilities = estimator.predict_proba(X)
-        
         out_dir_path = output_dir_path
         predictions = pd.DataFrame(probabilities)
         predictions.to_csv(out_dir_path + "predictions.tsv")
-
-        
-        
-        
-        
-        
-        
-        
-        
